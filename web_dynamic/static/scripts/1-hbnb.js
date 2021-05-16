bbcac9454c1a60eb437a79aabfaae8a6189bd951
Giraluna1 <lunagolo@hotmail.com>
2021-05-16 15:54:01 -0500

    add JQuery script

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,1 +1,14 @@
-alert('Hola');
+$(document).ready(function () {
+  let objs = {};
+  $('input').change(function () {
+    /* alert('Function input'); */
+    if ($(this).is(':checked')) {
+      objs[$(this).attr("data-name")] = $(this).attr("data-id");
+    } else if ($(this).is(':not(:checked)')) {
+      delete objs[$(this).attr("data-name")]
+    }
+    let content = Object.keys(objs)
+    console.log(content)
+    $(".amenities h4").text(content)
+  });
+});
